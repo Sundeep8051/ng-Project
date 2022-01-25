2d1f9c89b5b20b1851193701b2b116bed0b098ce
Sundeep Manibhushan <sundeepm1@gmail.com>
2022-01-25 13:31:30 +0530

    updated the components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Pongal", "A South Indian Dish", "../assets/pongal.jpg"),
+    new Recipe("Bisi Bele Bath", "A South Indian Dish", "../assets/bisi_bele_bath.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {
