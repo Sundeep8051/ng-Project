df136ebebd8d0eb3f391ead451ebe67ba09ecafc
Sundeep Manibhushan <sundeepm1@gmail.com>
2022-01-26 23:40:21 +0530

    updated the shopping list component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,9 +8,13 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe : Recipe;
+  @Output() recipeItemEvent = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(){
+    this.recipeItemEvent.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,9 +11,13 @@ export class RecipeListComponent implements OnInit {
     new Recipe("Pongal", "A South Indian Dish", "../assets/pongal.jpg"),
     new Recipe("Bisi Bele Bath", "A South Indian Dish", "../assets/bisi_bele_bath.jpg")
   ];
+  @Output() onRecipeListEvent = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeItemFired(recipe:Recipe){
+    this.onRecipeListEvent.emit(recipe);
+  }
 }
